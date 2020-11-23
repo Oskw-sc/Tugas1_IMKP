f6c2d48f3912cd5e19c048409f6b1c0d17630bc5
191402060 - Willioms Sanjaya <66860549+Oskw-sc@users.noreply.github.com>
2020-11-23 13:03:31 +0700

    update head.php

--- components/head.php
+++ components/head.php
@@ -11,6 +11,8 @@
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Parisienne&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;700&display=swap" rel="stylesheet">
+  <!-- Icons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <!-- Styles -->
   <link rel="stylesheet" href="assets/style.css">
   <title>Diary Aktivitas</title>

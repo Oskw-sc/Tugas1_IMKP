a0a2791b4ccada64b259e118ba65c861d3cb2955
191402072 Hendry Winata <hendry24680@gmail.com>
2020-11-23 11:50:20 +0700

    pembuatan tampilan dan penghapusan beberapa file yang telah digabung, masih perlu perbaikan pada query crud.

---
+++ components/foot.php
@@ -0,0 +1,19 @@
+  <footer class="bg-light py-4 text-center">
+    <a class="text-dark" href="https://www.usu.ac.id/id/" target="_blank" data-toggle="tooltip" data-placement="top" title="Website Resmi USU">
+      <img class="navbar-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Logo_of_North_Sumatra_University.svg/1024px-Logo_of_North_Sumatra_University.svg.png" height="40" width="40" alt="Logo USU">
+      <span class="align-middle h3">Universitas Sumatera Utara</span>
+    </a>
+    <p class="text-dark h5">&copy; 2020 Copyright</p>
+  </footer>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+  <script>
+    $(document).ready(function () {
+      $('#list-table').DataTable();
+    });
+  </script>
+</body>
+</html>
---
+++ components/head.php
@@ -0,0 +1,31 @@
+<?php include 'koneksi.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Plugins -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css">
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Parisienne&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;700&display=swap" rel="stylesheet">
+  <!-- Styles -->
+  <link rel="stylesheet" href="assets/style.css">
+  <title>Diary Aktivitas</title>
+</head>
+<body>
+  <nav class="navbar bg-light navbar-light px-3">
+      <a class="navbar-brand p-2" href="index.php">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Logo_of_North_Sumatra_University.svg/1024px-Logo_of_North_Sumatra_University.svg.png" alt="logo USU" width="60" height="60">
+        <span class="h2 align-middle ml-2 text-success strong-fontsize">Diary Mahasiswa</span>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end py-2" id="collapsibleNavbar">
+        <h4 class="text-muted light-fontsize">Di sini, Anda dapat berbagi cerita!!!</h4>
+        <h5>Cerita apa?? Cerita tentang kehidupan perkuliahan Anda-anda sebagai mahasiswa USU.</h5>
+      </div>  
+    </nav>
